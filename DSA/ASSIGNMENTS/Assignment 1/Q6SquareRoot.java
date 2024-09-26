815e144388929fe53b92e69e8f81e9fd0a5e56b5
kartikay Dhakad <127400297+kartikaydhakad07@users.noreply.github.com>
2024-09-26 23:47:26 +0530

    Add files via upload

---
+++ DSA/ASSIGNMENTS/Assignment 1/Q6SquareRoot.java
@@ -0,0 +1,31 @@
+public class Q6SquareRoot {
+
+  
+    public static int sqrt(int num, int low, int high) {
+        if (low > high) { 
+            return high; 
+        }
+
+        int mid = (low + high) / 2; 
+
+        
+        if (mid * mid == num) {
+            return mid; 
+        }
+
+        
+        if (mid * mid < num) {
+            return sqrt(num, mid + 1, high); // Guess bigger
+        }
+
+        
+        return sqrt(num, low, mid - 1); // Guess smaller
+    }
+
+    public static void main(String[] args) {
+        int number = 50; 
+        int result = sqrt(number, 0, number); 
+
+        System.out.println("The square root of " + number + " is: " + result); 
+    }
+}
